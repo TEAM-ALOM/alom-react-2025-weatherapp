3730cabb9044290d8c5aa471e8773a6484a1b0c9
jebiyeon02 <jhje5595@naver.com>
2026-01-07 19:29:30 +0900

    gd

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { Container } from "./components/styles/StyledComponents";
-import CurrentWeather from "./components/CurrentWeather";
-import HourlyForecast from "./components/HourlyForecast";
-import DailyForecast from "./components/DailyForecast";
+import React, { useState, useEffect } from 'react';
+import { Container } from './components/styles/StyledComponents';
+import CurrentWeather from './components/CurrentWeather';
+import HourlyForecast from './components/HourlyForecast';
+import DailyForecast from './components/DailyForecast';
 
 function App() {
   const API_URL =
-    "https://api.open-meteo.com/v1/forecast?latitude=37.566&longitude=126.9784&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max&timezone=Asia%2FTokyo&forecast_days=7";
+    'https://api.open-meteo.com/v1/forecast?latitude=37.566&longitude=126.9784&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max&timezone=Asia%2FTokyo&forecast_days=7';
 
   const [weatherData, setWeatherData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-
+  asdasdsa;
   useEffect(() => {
     fetch(API_URL)
       .then((response) => response.json())
@@ -19,7 +19,7 @@ function App() {
         setIsLoading(false);
       })
       .catch((error) => {
-        console.error("데이터 로드 실패:", error);
+        console.error('데이터 로드 실패:', error);
         setIsLoading(false);
       });
   }, []);
